7bb1fb9580b6123c59bf455dd6e239368360d030
baiyangx <350916161@qq.com>
2015-11-14 14:10:48 +0800

    Initial Commit

---
+++ ZOJ3610/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ZOJ3610
+//
+//  Created by frank on 15-11-14.
+//  Copyright (c) 2015年 frank. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
